41a32e4e7cae9b575718c9becf8fbc6dd0052425
evil-A <57819409+evil-A@users.noreply.github.com>
2021-10-26 18:03:32 +0200

    Add files via upload

--- micro_paint/REAL_SHIT.c
+++ micro_paint/REAL_SHIT.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: evila-ro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/26 12:48:50 by evila-ro          #+#    #+#             */
-/*   Updated: 2021/10/26 17:55:04 by evila-ro         ###   ########.fr       */
+/*   Created: 2021/10/25 12:35:46 by evila-ro          #+#    #+#             */
+/*   Updated: 2021/10/26 11:49:52 by evila-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,15 @@ int	ft_strlen(char *str)
 {
 	int	i;
 
-	i = 0;
-	while (str[i])
-		i++;
+	for (i = 0; str[i]; i++);
 	return (i);
 }
 
 int	draw(float x, float y, float xp, float yp, float w, float h)
 {
-	if ( x < xp || x > xp + w || y < yp || y > yp + h)
+	if (x < xp || x > xp + w || y < yp || y > yp + h)
 		return (0);
-	else if (x - xp < 1 || (xp + w) - x < 1 || y - yp < 1 || (yp + h) - y < 1)
+	else if (x - xp < 1.00000000 || (xp + w) - x < 1.00000000 || y - yp < 1.00000000 || (yp + h) - y < 1.00000000)
 		return (1);
 	return (2);
 }
@@ -38,32 +36,32 @@ int	main(int argc, char **argv)
 {
 	FILE	*file;
 	char	*sheet, back, type, fill;
-	int		wr, p, x, y, bw, bh;
+	int		line, p, x, y, bw, bh;
 	float	xp, yp, w, h;
 
 	if (argc != 2)
 		return (!(!(write(1, "Error: argument\n", 16))));
 	if (!(file = fopen(argv[1], "r")) ||
-	   (fscanf(file, "%i %i %c\n", &bw, &bh, &back) != 3) ||
-	   (!(bw > 0 && bw <= 300 && bh > 0 && bh <= 300)) ||
-	   (!(sheet = (char *)malloc(sizeof(char) * (bw * bh)))))
+		(fscanf(file, "%i %i %c\n", &bw, &bh, &back) != 3) || (!(bw > 0 && bw <= 300 && bh > 0 && bh <= 300)) || (!(sheet = (char *)malloc(sizeof(char) * (bw * bh)))))
 		return (!(!(write(1, "Error: Operation file corrupted\n", 32))));
 	memset(sheet, back, bw * bh);
-	while ((wr = fscanf(file, "%c %f %f %f %f %c\n", &type, &xp, &yp, &w, &h, &fill)) == 6)
+	while ((line = fscanf(file, "%c %f %f %f %f %c\n", &type, &xp, &yp, &w, &h, &fill)) == 6)
 	{
 		if (!(w > 0 && h > 0) || !(type == 'R' || type == 'r'))
+		{
 			break ;
+		}
 		for (x = 0; x < bw; x++)
 		{
 			for (y = 0; y < bh; y++)
 			{
 				p = draw((float)x, (float)y, xp, yp, w, h);
-				if (p == 1 || (p == 2 && type == 'R'))
+				if ( p == 1 || (p == 2 && type == 'R'))
 					sheet[y * bw + x] = fill;
 			}
 		}
 	}
-	if (wr != -1)
+	if (line != -1)
 	{
 		free(sheet);
 		return (!(!(write(1, "Error: Operation file corrupted\n", 32))));
--- micro_paint/SHIT.c
+++ micro_paint/SHIT.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: evila-ro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/26 12:48:50 by evila-ro          #+#    #+#             */
-/*   Updated: 2021/10/26 17:55:04 by evila-ro         ###   ########.fr       */
+/*   Created: 2021/10/25 10:58:08 by evila-ro          #+#    #+#             */
+/*   Updated: 2021/10/26 12:35:51 by evila-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,15 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
+int	terror(char *str)
+{
+	write(1, str, ft_strlen(str));
+	return (1);
+}
+
 int	draw(float x, float y, float xp, float yp, float w, float h)
 {
-	if ( x < xp || x > xp + w || y < yp || y > yp + h)
+	if (x < xp || x > xp + w || y < yp || y > yp + h)
 		return (0);
 	else if (x - xp < 1 || (xp + w) - x < 1 || y - yp < 1 || (yp + h) - y < 1)
 		return (1);
@@ -37,41 +43,58 @@ int	draw(float x, float y, float xp, float yp, float w, float h)
 int	main(int argc, char **argv)
 {
 	FILE	*file;
-	char	*sheet, back, type, fill;
-	int		wr, p, x, y, bw, bh;
-	float	xp, yp, w, h;
+	char	*sheet;
+	char	back;
+	char	type;
+	char	fill;
+	int		line;
+	int		p;
+	int		x;
+	int		y;
+	int		bw;
+	int		bh;
+	float	xp;
+	float	yp;
+	float	w;
+	float	h;
 
 	if (argc != 2)
-		return (!(!(write(1, "Error: argument\n", 16))));
+		return (terror("Error: argument\n"));
 	if (!(file = fopen(argv[1], "r")) ||
-	   (fscanf(file, "%i %i %c\n", &bw, &bh, &back) != 3) ||
-	   (!(bw > 0 && bw <= 300 && bh > 0 && bh <= 300)) ||
-	   (!(sheet = (char *)malloc(sizeof(char) * (bw * bh)))))
-		return (!(!(write(1, "Error: Operation file corrupted\n", 32))));
+		(fscanf(file, "%i %i %c\n", &bw, &bh, &back) != 3) ||
+		(!(bw > 0 && bw <= 300 && bh > 0 && bh <= 300)) ||
+		(!(sheet = (char *)malloc(sizeof(char) * (bw * bh)))))
+		return (terror("Error: Operation file corrupted\n"));
 	memset(sheet, back, bw * bh);
-	while ((wr = fscanf(file, "%c %f %f %f %f %c\n", &type, &xp, &yp, &w, &h, &fill)) == 6)
+	while ((line = fscanf(file, "%c %f %f %f %f %c\n", &type, &xp, &yp, &w, &h, &fill)) == 6)
 	{
 		if (!(w > 0 && h > 0) || !(type == 'R' || type == 'r'))
 			break ;
-		for (x = 0; x < bw; x++)
+		x = 0;
+		while (x < bw)
 		{
-			for (y = 0; y < bh; y++)
+			y = 0;
+			while (y < bh)
 			{
 				p = draw((float)x, (float)y, xp, yp, w, h);
 				if (p == 1 || (p == 2 && type == 'R'))
 					sheet[y * bw + x] = fill;
+				y++;
 			}
+			x++;
 		}
 	}
-	if (wr != -1)
+	if (line != -1)
 	{
 		free(sheet);
-		return (!(!(write(1, "Error: Operation file corrupted\n", 32))));
+		return (terror("Error: Operation file corrupted\n"));
 	}
-	for (y = 0; y < bh; y++)
+	y = 0;
+	while (y < bh)
 	{
 		write(1, sheet + y * bw, bw);
 		write(1, "\n", 1);
+		y++;
 	}
 	free(sheet);
 	fclose(file);
